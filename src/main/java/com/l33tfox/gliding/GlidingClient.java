2fbea9a0f8cacb97f68214e99ca54f83ddac8718
L33tfox <leetfox8@gmail.com>
2024-12-18 16:29:30 -0500

    Initial commit

---
+++ src/main/java/com/l33tfox/gliding/GlidingClient.java
@@ -0,0 +1,11 @@
+package com.l33tfox.gliding;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlidingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/l33tfox/gliding/GlidingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.l33tfox.gliding;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GlidingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
